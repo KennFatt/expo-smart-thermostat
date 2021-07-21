30a1da4d3463fd7d6fb84018695b45eb44bdfe33
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-07-21 13:04:07 +0700

    What's wrong with me...

--- App.js
+++ App.js
@@ -39,7 +39,6 @@ export default function App() {
                   />
                 ),
               }}
-              tab
             />
             <Tab.Screen
               name="Actuators"
